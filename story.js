0e9571ef3f940a317a71d78fd2424fb7caa080a1
Lailz <lailaabdulraheem@gmail.com>
2021-01-10 14:11:09 +0300

    task done.. yaaaayyy

---
+++ story.js
@@ -0,0 +1,11 @@
+const name = "Laila";
+const age = 250;
+const hobby = "playing piano";
+
+console.log(
+  `Mrs. ${name}, in 33 years, you will be ${
+    age + 33
+  } years old. By then you'll have decades of ${hobby} practice!`
+);
+
+// Mr./Mrs. NAME, in 33 years, you will be AGE_PLUS_33 years old. By then you'll have decades of HOBBY practice!
